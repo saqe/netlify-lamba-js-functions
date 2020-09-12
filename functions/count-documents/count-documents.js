bc263cff8d3cb5735776ed432ade9aaf013213ba
SaqibAleem <saqib.py@gmail.com>
2020-09-13 01:52:22 +0500

    removed async

--- functions/count-documents/count-documents.js
+++ functions/count-documents/count-documents.js
@@ -2,16 +2,18 @@ const mongo = require('mongodb');
 const superagent = require('superagent');
 
 exports.handler = async (event, context) => {
+    console.log("in Handler");
+    console.log("trying to connect");
     mongo.MongoClient
         .connect(process.env.MONGODB_URI,
             function (err, database) {
-                await database
+                database
                     .db('Hifi')
                     .collection("records")
                     .countDocuments({ status: { $exists: false } })
                     .then(function (result) {
                         console.log(result);
-                        await superagent.get(process.env.MESSAGE_API + result)
+                        superagent.get(process.env.MESSAGE_API + result)
                             .then(res => {
                                 console.log(res);
                             })
