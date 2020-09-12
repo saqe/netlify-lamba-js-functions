47fcc53ccf50fac4887abd61ac7b99996b2c9e52
SaqibAleem <saqib.py@gmail.com>
2020-09-13 01:46:35 +0500

    added await

--- functions/count-documents.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongodb');
-
-exports.handler = async (event, context) => {
-    mongo.MongoClient
-    return { 
-        statusCode : 200, 
-        body : "Connection with database is done!"
-    };
-};
---
+++ functions/count-documents/count-documents.js
@@ -0,0 +1,23 @@
+const mongo = require('mongodb');
+const superagent = require('superagent');
+
+exports.handler = async (event, context) => {
+    mongo.MongoClient
+        .connect(process.env.MONGODB_URI,
+            function (err, database) {
+                await database
+                    .db('Hifi')
+                    .collection("records")
+                    .countDocuments({ status: { $exists: false } })
+                    .then(function (result) {
+                        console.log(result);
+                        await superagent.get(process.env.MESSAGE_API + result)
+                            .then(res => {
+                                console.log;
+                            })
+                            .catch(err => {
+                                // err.message, err.response
+                            });
+                    });
+            });
+};
